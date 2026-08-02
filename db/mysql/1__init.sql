abc369b84405553d71cb1df1a1210faca0db53be
ant3k <antek@mail.ru>
2026-08-02 20:31:57 +0700

    fix: user right migration

--- db/mysql/1__init.sql
+++ db/mysql/1__init.sql
@@ -1,15 +1,87 @@
-CREATE TABLE meshrouter.packets (dbtime TIMESTAMP, id BIGINT, src BIGINT, dst BIGINT, chHash INT,
-  hopLimit INT, hopStart INT, nextHop INT, relayNode INT, rssi FLOAT, snr FLOAT(7,2), transport INT,
-  isTX BOOLEAN, pSize INT, isMQTT BOOLEAN);
+-- meshrouter.chat definition
 
-CREATE TABLE meshrouter.info (id BIGINT, sname text, lname text,
-  longitude DOUBLE, latitude DOUBLE, altitude FLOAT(7,2), isMobile BOOLEAN, role INT,
-  lastHeard TIMESTAMP, isIgnored BOOLEAN, isFavored BOOLEAN);
+CREATE TABLE `chat` (
+  `dbtime` timestamp NULL DEFAULT NULL,
+  `id` bigint DEFAULT NULL,
+  `src` bigint DEFAULT NULL,
+  `dst` bigint DEFAULT NULL,
+  `message` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE meshrouter.neighbours (dbtime TIMESTAMP, src BIGINT, rssi FLOAT, snr FLOAT(7,2));
+-- meshrouter.info definition
 
-CREATE TABLE meshrouter.names (id BIGINT, lname TEXT, sname TEXT);
+CREATE TABLE `info` (
+  `id` bigint DEFAULT NULL,
+  `sname` text,
+  `lname` text,
+  `longitude` double DEFAULT NULL,
+  `latitude` double DEFAULT NULL,
+  `altitude` float(7,2) DEFAULT NULL,
+  `isMobile` tinyint(1) DEFAULT NULL,
+  `role` int DEFAULT NULL,
+  `lastHeard` timestamp NULL DEFAULT NULL,
+  `isIgnored` tinyint(1) DEFAULT NULL,
+  `isFavored` tinyint(1) DEFAULT NULL,
+  KEY `idx_id_lastheard` (`id`,`lastHeard` DESC),
+  KEY `idx_coords` (`longitude`,`latitude`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE meshrouter.chat (dbtime TIMESTAMP, id BIGINT, src BIGINT, dst BIGINT, message TEXT);
+-- meshrouter.neighbours definition
 
-CREATE TABLE meshrouter.badpkts (dbtime TIMESTAMP, pSize INT);
+CREATE TABLE `neighbours` (
+  `dbtime` timestamp NULL DEFAULT NULL,
+  `src` bigint DEFAULT NULL,
+  `rssi` float DEFAULT NULL,
+  `snr` float(7,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- meshrouter.packets definition
+
+CREATE TABLE `packets` (
+  `dbtime` timestamp NULL DEFAULT NULL,
+  `id` bigint DEFAULT NULL,
+  `src` bigint DEFAULT NULL,
+  `dst` bigint DEFAULT NULL,
+  `chHash` int DEFAULT NULL,
+  `hopLimit` int DEFAULT NULL,
+  `hopStart` int DEFAULT NULL,
+  `nextHop` int DEFAULT NULL,
+  `relayNode` int DEFAULT NULL,
+  `rssi` float DEFAULT NULL,
+  `snr` float(7,2) DEFAULT NULL,
+  `transport` int DEFAULT NULL,
+  `isTX` tinyint(1) DEFAULT NULL,
+  `pSize` int DEFAULT NULL,
+  `isMQTT` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- meshrouter.traces definition
+
+CREATE TABLE `traces` (
+  `id` bigint NOT NULL,
+  `dbtime` datetime NOT NULL,
+  `src` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dst` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `snr` decimal(5,2) NOT NULL,
+  `is_reverse` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+GRANT ALTER ON meshrouter.* TO 'meshrouter'@'%';
+GRANT CREATE ON meshrouter.* TO 'meshrouter'@'%';
+GRANT CREATE VIEW ON meshrouter.* TO 'meshrouter'@'%';
+GRANT DELETE ON meshrouter.* TO 'meshrouter'@'%';
+GRANT DROP ON meshrouter.* TO 'meshrouter'@'%';
+GRANT GRANT OPTION ON meshrouter.* TO 'meshrouter'@'%';
+GRANT INDEX ON meshrouter.* TO 'meshrouter'@'%';
+GRANT INSERT ON meshrouter.* TO 'meshrouter'@'%';
+GRANT REFERENCES ON meshrouter.* TO 'meshrouter'@'%';
+GRANT SELECT ON meshrouter.* TO 'meshrouter'@'%';
+GRANT SHOW VIEW ON meshrouter.* TO 'meshrouter'@'%';
+GRANT TRIGGER ON meshrouter.* TO 'meshrouter'@'%';
+GRANT UPDATE ON meshrouter.* TO 'meshrouter'@'%';
+GRANT ALTER ROUTINE ON meshrouter.* TO 'meshrouter'@'%';
+GRANT CREATE ROUTINE ON meshrouter.* TO 'meshrouter'@'%';
+GRANT CREATE TEMPORARY TABLES ON meshrouter.* TO 'meshrouter'@'%';
+GRANT EXECUTE ON meshrouter.* TO 'meshrouter'@'%';
+GRANT LOCK TABLES ON meshrouter.* TO 'meshrouter'@'%';
+GRANT GRANT OPTION ON meshrouter.* TO 'meshrouter'@'%';
